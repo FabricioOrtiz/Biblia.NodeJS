7c0ea8ef04c45d2843bf13af41f887d01387d38a
Tito Ortiz CR <fabricio.ortiz.alfaro@gmail.com>
2024-06-12 16:01:49 -0600

    adding more lessons

---
+++ 04-bases-node/app.js
@@ -0,0 +1,14 @@
+//5 * 1 = 5
+//5 * 2 = 10
+//.
+//print table of 5
+console.clear(); 
+(() => {
+    const base = 5;
+    for (let index = 1; index <= 10; index++) {
+        console.log(`${ base } x ${ index } = ${base*index}`);
+        
+     }
+ })();
+ 
+
